08c89e971d7272d24e99c28571ef8301e89a3a05
rorygwozdz <rory.gwozdz@gmail.com>
2019-01-03 15:12:15 -0800

    moved some folders around

--- finance/mastering_python_for_finance/dictionary_practice.py
+++
@@ -1,5 +0,0 @@
-dictionary = {'derka': 15, 'd': 1, 'e': 3, 'r': 4, 'k': 5, 'a': 2}
-print(dictionary.items())
-print(dictionary.keys())
-print(dictionary.has_key('derka'))
-print(dictionary['d'])
--- finance/mastering_python_for_finance/socket_grabber.py
+++
@@ -1,8 +0,0 @@
-import socket
-print(socket)
-socket.setdefaulttimeout(2)
-s = socket.socket()
-s.connect(("192.168.95.148",21))
-ans = s.recv(1024)
-print ans
-#220 FreeFloat Ftp Server (Version 1.00).
